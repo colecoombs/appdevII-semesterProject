c7aade1ab2f0c222bed80c15a4516a8b27ccf3a7
Cole <ccoombs002@drury.edu>
2023-03-22 20:49:28 -0500

    removed unused calls

--- components/Login.js
+++ components/Login.js
@@ -22,50 +22,6 @@ import {
 import * as SecureStore from "expo-secure-store";
 import axios from "axios";
 
-/*  async function Login(email, password) {
-	console.log("TEST");
-	const response = await fetch("http:/10.15.5.49:3000/api/user/auth", {
-		method: "POST",
-		body: {
-			'email': email,
-			'password': password,
-		},
-	})
-		.then(async () => {
-			if (response) {
-				console.log("then function")
-				const tokenResponse = await response.json();
-				SecureStore.setItemAsync("token", tokenResponse.token);
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-}  */
-
- /* export function Login(email, password) {
-	const data = {
-		'email': email,
-		'password': password,
-	};
-
-	axios({
-		method: "POST",
-		url: "http:/10.15.5.49:3000/api/user/auth",
-		//url: "localhost:3000/api/user/auth",
-		data: data,
-		headers: { "Content-Type": "application/x-ww-form-urlencoded" },
-	})
-		.then(async (res) => {
-			console.log(JSON.stringify(res.data));
-			//await SecureStore.setItemAsync("email", res.data.email);
-			await SecureStore.setItemAsync("token", res.data.token);
-		})
-		.catch((err) => {
-			console.log(JSON.stringify(err));
-		});
-} */
-
  export function Login(email, password) {
 	axios.post('http:/10.15.5.49:3000/api/user/auth',{
 		'email': email,
